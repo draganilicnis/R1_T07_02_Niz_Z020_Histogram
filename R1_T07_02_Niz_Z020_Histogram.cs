391d27674db0fadfa4eb6188fb8cdc12e62047e2
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-11-12 10:26:12 +0100

    Update R1_T07_02_Niz_Z020_Histogram.cs

--- R1_T07_02_Niz_Z020_Histogram.cs
+++ R1_T07_02_Niz_Z020_Histogram.cs
@@ -4,6 +4,8 @@
 // https://arena.petlja.org/competition/r1-07-nizovi-05-preslikavanja#tab_132994
 // https://arena.petlja.org/competition/r1-07-nizovi-04-vektori-polinomi#tab_132710
 // https://arena.petlja.org/competition/skola-od-kuce-nizovi1-zadaci#tab_97594
+// https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#BFormatString
+// https://github.com/draganilicnis/R1_T07_02_Niz_Z020_Histogram/blob/main/R1_T07_02_Niz_Z020_Histogram.cs
 // 1 WA
 
 using System;
@@ -30,14 +32,14 @@ class R1_T07_02_Niz_Z020_Histogram_01_WA_Ver_000
 
         // Ispisujemo histogram
         double t = a;
-        foreach (int h in histogram)
+        for (int i = 0; i < n; i++)
         {
             Console.Write("[" + t.ToString("F3") + ", " + (t + dx).ToString("F3") + "): ");             // Ispisujemo granice tekuceg podeoka (zaokruzene na tri decimale)
-            Console.Write(h + "\t");                                                                    // Ispisujemo broj tacaka u tekucem podeoku
+            Console.Write(histogram[i] + "\t");                                                         // Ispisujemo broj tacaka u tekucem podeoku
             // Ispisujemo zvezdice
-            double procenat = (double)h / (double)k;
+            double procenat = (double)histogram[i] / (double)k;
             int brojZvezdica = (int)Math.Round(100 * procenat);
-            for (int i = 0; i < brojZvezdica; i++) Console.Write("*");
+            for (int z = 0; z < brojZvezdica; z++) Console.Write("*");
             Console.WriteLine();
             t = t + dx;        // Prelazimo na naredni podeok
         }
